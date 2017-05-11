a3549a738ad51f1d3fb0000fd798245829f99518
lhd19920802 <414320737@qq.com>
2017-05-11 10:09:07 +0800

    集成了极光推送，实现了应用的推送功能

--- app/src/main/java/com/lhd/mynews/MyApplication.java
+++ app/src/main/java/com/lhd/mynews/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import org.xutils.x;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by lihuaidong on 2017/5/3 11:14.
  * 微信：lhd520ssp
@@ -18,5 +20,8 @@ public class MyApplication extends Application
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
     }
 }
